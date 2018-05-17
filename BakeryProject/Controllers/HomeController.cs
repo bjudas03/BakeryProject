45341f3fdab7aeef412749db0581f867c976dd5e
Tom Brady <bradyt2215@gmail.com>
2018-05-17 10:45:58 -0700

    change

--- BakeryProject/Controllers/HomeController.cs
+++ BakeryProject/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//mines works better than Micah's...now
 namespace BakeryProject.Controllers
 {
     public class HomeController : Controller
