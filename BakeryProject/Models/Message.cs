5e97922475a213005d72be582cd31dce47657e54
Tom Brady <bradyt2215@gmail.com>
2018-05-22 11:31:03 -0700

    Register/NewPerson

---
+++ BakeryProject/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeryProject.Models
+{
+    public class Message
+    {
+        public Message() { }
+
+        public Message(string msg)
+        {
+            MessageText = msg;
+        }
+        public string MessageText { get; set; }
+    }
+}
