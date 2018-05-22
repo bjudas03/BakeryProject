176ddfad947c9857dd4eb406cd75bdc6fce64253
Tom Brady <bradyt2215@gmail.com>
2018-05-22 11:12:55 -0700

    new changes

---
+++ BakeryProject/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryProject.Models;
+
+
+namespace BakeryProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            //initialize entities classes
+            BakeryEntities db = new BakeryEntities();
+            //pass the collection categories to the index as a list
+            return View(db.Products.ToList());
+        }
+    }
+}
