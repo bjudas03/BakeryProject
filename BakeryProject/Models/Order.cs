6fd23189499dd87a1ec72708cfe75b7a87f38194
Tom Brady <bradyt2215@gmail.com>
2018-05-31 11:29:45 -0700

    update to order controller

--- BakeryProject/Models/Order.cs
+++ BakeryProject/Models/Order.cs
@@ -7,5 +7,9 @@ namespace BakeryProject.Models
 {
     public class Order
     {
+        public int ProductKey{ get; set; }
+        public string ProductName { get; set; }
+        public decimal PricePrice { get; set; }
+        public int SaleDetailQuantity { get; set; }
     }
 }
